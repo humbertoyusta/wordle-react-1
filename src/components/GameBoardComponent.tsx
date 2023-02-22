9bbdb6f0f2a0a89d4b52cd7bef7c23ef41a0305a
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-02-22 19:29:36 +0100

    feat/adding-status-to-letters

--- src/components/GameBoardComponent.tsx
+++ src/components/GameBoardComponent.tsx
@@ -1,25 +1,76 @@
 import React from "react";
 import WordComponent from "./WordComponent";
 import {useGameBoardContext} from "../contexts/GameBoardProvider";
+import useKeypress from "../hooks/useKeypress";
+import GameBoardHelper from "../helpers/GameBoardHelper";
 
 type GameBoardState = {
     wordGuesses: String[],
+    wordGuessesStatus: Number[][],
+    currentGuess: String,
 }
 
 export default function GameBoardComponent() {
     const [state, setState] = React.useState<GameBoardState>({
         wordGuesses: [],
+        wordGuessesStatus: [],
+        currentGuess: "",
     });
 
-    // List all previous guesses
-    const words = state.wordGuesses.map((word: String) => <WordComponent word={word}></WordComponent>);
+    // Add a character to currentGuess when a key is pressed
+    useKeypress((key: string) => {
+        if (key.length === 1) {
+            let charToAppend = key.toUpperCase();
+            setState({...state, currentGuess: state.currentGuess + charToAppend});
+        }
+    });
+
+    // Add the currentGuess to wordGuesses when the enter key is pressed
+    const correctWord = useGameBoardContext().correctWord;
+    useKeypress((key: string) => {
+        if (key === "Enter") {
+
+            if (!GameBoardHelper.isWordValidLength(state.currentGuess, correctWord))
+                alert("Word must be the same length as the correct word");
+
+            if (!GameBoardHelper.isWordNotGuessed(state.currentGuess, state.wordGuesses))
+                alert("Word has already been guessed");
+
+            const currentGuessStatus = GameBoardHelper.getWordStatus(state.currentGuess, correctWord);
+
+            setState({
+                ...state,
+                wordGuesses: [...state.wordGuesses, state.currentGuess],
+                currentGuess: "",
+                wordGuessesStatus: [...state.wordGuessesStatus, currentGuessStatus],
+            });
+        }
+    });
+
+    // Remove the last character from currentGuess when the backspace key is pressed
+    useKeypress((key: string) => {
+        if (key === "Backspace")
+            setState({...state, currentGuess: state.currentGuess.slice(0, -1)});
+    });
 
     // get maxGuesses from the context
     const maxGuesses = useGameBoardContext().maxGuesses;
 
+    // Create a counter to use as a key for each word component
+    let counter = 0;
+
+    // List all previous guesses
+    const words = state.wordGuesses.map(
+        (word: String, index: number) =>
+            <WordComponent key={counter ++} word={word} status={state.wordGuessesStatus[index]} />
+    );
+
+    // Add the current guess
+    words.push(<WordComponent key={counter ++} word={state.currentGuess} status={[]} />);
+
     // Add empty words to fill the remaining guesses
-    for (let i = 0; i < maxGuesses.valueOf() - state.wordGuesses.length; i++)
-        words.push(<WordComponent word={""} />);
+    while (words.length < maxGuesses)
+        words.push(<WordComponent key={counter ++} word={""} status={[]} />);
 
     return (
         <div className="flex-col flex justify-between flex-nowrap m-auto">
--- src/components/WordComponent.tsx
+++ src/components/WordComponent.tsx
@@ -2,7 +2,7 @@ import {LetterCellStatusEnum} from "../enums/LetterCellStatusEnum";
 import LetterCellComponent from "./LetterCellComponent";
 import {useGameBoardContext} from "../contexts/GameBoardProvider";
 
-export default function WordComponent({word}: {word: String}) {
+export default function WordComponent({word, status}: {word: String, status: Number[]}) {
 
     // Get the required length of the word from the context
     const requiredLength = useGameBoardContext().correctWord.length;
@@ -10,12 +10,15 @@ export default function WordComponent({word}: {word: String}) {
     // Create a counter to use as a key for each letter cell
     let counter = 0;
 
+    // Add empty status values to fill the remaining letters
+    while (status.length < requiredLength)
+        status.push(LetterCellStatusEnum.EMPTY);
+
     // Create a letter cell for each letter in the word
-    const letters =
-        Array.from(word).map(
-            (letter: String) =>
-                <LetterCellComponent key={counter ++} letter={letter} status={LetterCellStatusEnum.CORRECT} />
-        );
+    const letters: JSX.Element[] = [];
+    for (let i = 0; i < word.length; i++) {
+        letters.push(<LetterCellComponent key={counter ++} letter={word[i]} status={status[i]} />)
+    }
 
     // Add empty letter cells to fill the remaining letters
     while (letters.length < requiredLength)
