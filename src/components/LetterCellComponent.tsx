6ff60f2d647c64833c49fb70574176a2913f4946
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-02-23 00:27:59 +0100

    refactor/use-map-for-conditional-styles

--- src/components/LetterCellComponent.tsx
+++ src/components/LetterCellComponent.tsx
@@ -2,20 +2,15 @@ import {LetterCellStatusEnum} from "../enums/LetterCellStatusEnum";
 
 export default function LetterCellComponent({letter, status}: {letter: String, status: Number}) {
 
-    // Create a list of style classes to apply to the letter cell
-    const statusStyleClasses: String[] = [];
-
-    // Add the appropriate style class based on the status
-    if (status === LetterCellStatusEnum.CORRECT)
-        statusStyleClasses.push("bg-green-400");
-    if (status === LetterCellStatusEnum.BAD_POSITION)
-        statusStyleClasses.push("bg-yellow-400");
-    if (status === LetterCellStatusEnum.INCORRECT)
-        statusStyleClasses.push("bg-red-400");
+    // Map of status to style classes
+    const statusStyleClasses = new Map<Number, String>();
+    statusStyleClasses.set(LetterCellStatusEnum.CORRECT, "bg-green-400");
+    statusStyleClasses.set(LetterCellStatusEnum.BAD_POSITION, "bg-yellow-400");
+    statusStyleClasses.set(LetterCellStatusEnum.INCORRECT, "bg-red-400");
 
     return (
         <div className="border-gray-900 border-2 w-10 h-10">
-            <p className={"text-center "+statusStyleClasses.toString().replace(',', ' ')}>{letter}</p>
+            <p className={"text-center "+statusStyleClasses.get(status)}>{letter}</p>
         </div>
     );
 }
