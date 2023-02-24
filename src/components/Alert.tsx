0d640ab14ba2ec053b7ecf68034cbbd2ac23b0ec
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-02-24 23:22:29 +0100

    feat/animate-alert

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,19 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import {AlertDiv} from "../styledComponents/AlertDiv";
 
+const ALERT_DURATION = 1000;
+
 export default function Alert ({message, color}: {message: String, color: String}) {
     const [visible, setVisible] = useState(true);
+    const [animation, setAnimation] = useState("hide");
 
     useEffect(() => {
-        const timer = setTimeout(() => {
-            setVisible(false);
-        }, 1000);
+        setAnimation("show");
+        const timer1 = setTimeout(() => {
+            setAnimation("hide");
+            }, ALERT_DURATION - 200);
+        const timer2 = setTimeout(() => {
+           setVisible(false);
+        }, ALERT_DURATION);
 
-        return () => clearTimeout(timer);
+        return () => {clearTimeout(timer1); clearTimeout(timer2);};
     }, []);
 
     return visible ? (
-        <AlertDiv color={color.valueOf()}>
+        <AlertDiv className={animation} color={color.valueOf()}>
             <p>{message}</p>
         </AlertDiv>
     ) : null;
--- src/styledComponents/AlertDiv.ts
+++ src/styledComponents/AlertDiv.ts
@@ -5,12 +5,22 @@ export const AlertDiv = styled.div<{color: String}>`
   top: 10%;
   left: 50%;
   border-radius: 10px;
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, -60%);
   background-color: ${props => props.color === "red" ? "#ffb3b3" : "#d4ffbc"};
   color: #000;
   padding: 16px;
   border: 1px solid #ccc;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  /* add a transition that makes the alert come from above and fade in */
+  transition: all 0.3s ease-in-out;
+  opacity: 0;
+  visibility: hidden;
+  /* make the alert visible and fade it in */
+  &.show {
+    transform: translate(-50%, -50%);
+    opacity: 1;
+    visibility: visible;
+  }
   & p {
     margin: 0 0 8px;
   }
