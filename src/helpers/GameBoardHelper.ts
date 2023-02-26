4a126b3f9c6feac178ebf5dd1a3f0be80cc4e6ea
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-02-26 20:30:54 +0100

    feat/use-helper-function-to-reveal-new-position

--- src/helpers/GameBoardHelper.ts
+++ src/helpers/GameBoardHelper.ts
@@ -6,7 +6,8 @@ export default class GameBoardHelper {
         const status: Number[] = [];
         const frequencyOfNotCorrect = new Map<String, Number>();
 
-        // Check if the letter is in the correct position
+        // Check if the letter is revealed, if not,
+        // check if it is in the correct position
         // If it isn't, add it to the frequency map
         for (let i = 0; i < word.length; i++) {
             if (revealedPositions.includes(i)) {
@@ -23,6 +24,7 @@ export default class GameBoardHelper {
         // Check if the letter is in the word, but in the wrong position
         // If it isn't, mark it as incorrect
         for (let i = 0; i < word.length; i++) {
+            // If the letter is already correct or revealed, skip it
             if (status[i] === LetterCellStatusEnum.CORRECT ||
                 status[i] === LetterCellStatusEnum.REVEALED) {
                 continue;
@@ -54,7 +56,14 @@ export default class GameBoardHelper {
         return !guessedWords.includes(word);
     }
 
-    static newLetterStatus(oldStatus: Map<String, Number>, word: String, wordStatus: Number[]): Map<String, Number> {
+    // Get the new letter status of the letters in the keyboard after a guess
+    static newLetterStatus(
+        oldStatus: Map<String, Number>,
+        word: String,
+        wordStatus: Number[],
+        revealedPositions: Number[],
+        correctWord: String
+    ): Map<String, Number> {
         const newStatus = new Map<String, Number>(oldStatus);
         for (let i = 0; i < word.length; i++) {
             if (wordStatus[i] === LetterCellStatusEnum.CORRECT) {
@@ -70,6 +79,47 @@ export default class GameBoardHelper {
                     newStatus.set(word[i], LetterCellStatusEnum.INCORRECT);
             }
         }
+        for (let revealPosition of revealedPositions) {
+            newStatus.set(correctWord[revealPosition.valueOf()], LetterCellStatusEnum.REVEALED);
+        }
         return newStatus;
+    }
+
+    // Check if a letter has been guessed before
+    static hasBeenGuessed(position: Number, wordGuessedStatus: Number[][]): boolean {
+        return wordGuessedStatus.some((status: Number[]) =>
+            status[position.valueOf()] === LetterCellStatusEnum.CORRECT);
+    }
+
+    // Get a random position that hasn't been guessed yet
+    static revealRandomPosition(revealedPositions: Number[], wordGuessesStatus: Number[][], correctWord: String): Number {
+        // Get all the possible positions that haven't been guessed or revealed yet
+        const possiblePositions = [];
+        for (let i = 0; i < correctWord.length; i++) {
+            if (revealedPositions.includes(i)) {
+                continue;
+            }
+            if (!GameBoardHelper.hasBeenGuessed(i, wordGuessesStatus)) {
+                possiblePositions.push(i);
+            }
+        }
+        // If there are no possible positions, get all the positions that haven't been revealed
+        if (possiblePositions.length === 0) {
+            for (let i = 0; i < correctWord.length; i++)
+                if (!revealedPositions.includes(i))
+                    possiblePositions.push(i);
+        }
+        // Return a random position from the possible positions
+        return possiblePositions[Math.floor(Math.random() * possiblePositions.length)];
+    }
+
+    // Get the new guess with the revealed letter
+    static getNewGuess(currentGuess: String, correctWord: String, newPosition: Number): String {
+        if (currentGuess.length > newPosition.valueOf())
+            return currentGuess.slice(0, newPosition.valueOf()) +
+                correctWord[newPosition.valueOf()] +
+                currentGuess.slice(newPosition.valueOf() + 1);
+        else
+            return currentGuess;
     }
 }
