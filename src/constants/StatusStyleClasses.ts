fa4b49ab608f2d6df40b64947ee830b3dfa99f16
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-02-26 20:28:48 +0100

    delete/unused-code

--- src/constants/StatusStyleClasses.ts
+++
@@ -1,8 +0,0 @@
-import {LetterCellStatusEnum} from "../enums/LetterCellStatusEnum";
-
-const StatusStyleClasses = new Map<Number, String>();
-StatusStyleClasses.set(LetterCellStatusEnum.CORRECT, "bg-green-400");
-StatusStyleClasses.set(LetterCellStatusEnum.BAD_POSITION, "bg-yellow-400");
-StatusStyleClasses.set(LetterCellStatusEnum.INCORRECT, "bg-red-400");
-
-export default StatusStyleClasses;
