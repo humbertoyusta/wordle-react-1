396b254b7647d3b3c5492e036e1c14e7f706f3b0
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-02-26 20:29:00 +0100

    fix/add-word-list-div

---
+++ src/styledComponents/WordListDiv.ts
@@ -0,0 +1,11 @@
+/*flex-col flex justify-between flex-nowrap m-auto*/
+
+import styled from "styled-components";
+
+export const WordListDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    flex-wrap: nowrap;
+    margin: auto;
+`;
