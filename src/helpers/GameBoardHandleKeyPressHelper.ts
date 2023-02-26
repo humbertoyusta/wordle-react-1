708591a33187f949e2eadb2fa37212c76761653d
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-02-26 20:29:55 +0100

    refactor/organize-and-comment-code

--- src/helpers/GameBoardHandleKeyPressHelper.ts
+++ src/helpers/GameBoardHandleKeyPressHelper.ts
@@ -7,9 +7,12 @@ const wordExists = require("word-exists");
 export class GameBoardHandleKeyPressHelper {
     private static handleLetterKeyPress(key: String, oldState: GameBoardState, correctWord: String) {
         let newGuess = oldState.currentGuess.slice();
+        // If the next letter is revealed, add it to the guess
         while (oldState.revealedPositions.includes(newGuess.length))
             newGuess += correctWord[newGuess.length];
+        // Add the pressed letter to the guess
         newGuess += key.toUpperCase();
+        // If the next letter is revealed, add it to the guess
         while (oldState.revealedPositions.includes(newGuess.length))
             newGuess += correctWord[newGuess.length];
         return {
@@ -20,9 +23,11 @@ export class GameBoardHandleKeyPressHelper {
 
     private static handleBackspaceKeyPress(oldState: GameBoardState): GameBoardState {
         let newGuess = oldState.currentGuess.slice();
+        // While the last letter is revealed, remove it
         while (oldState.revealedPositions.includes(newGuess.length - 1)) {
             newGuess = newGuess.slice(0, -1);
         }
+        // Remove the last letter
         return {
             ...oldState,
             currentGuess: newGuess.slice(0, -1),
@@ -30,30 +35,43 @@ export class GameBoardHandleKeyPressHelper {
     }
 
     private static handleEnterKeyPress(oldState: GameBoardState, correctWord: String): GameBoardState {
+        // If the guess is empty, throw an error
         if (!GameBoardHelper.isWordValidLength(oldState.currentGuess, correctWord))
             throw new AlertKeyPressError("Not enough letters.");
 
-        //if (!wordExists(oldState.currentGuess))
-        //    throw new AlertKeyPressError("Word must exist.");
+        // If the guess is not a word, throw an error
+        if (!wordExists(oldState.currentGuess))
+          throw new AlertKeyPressError("Word must exist.");
 
+        // If the guess has already been guessed, throw an error
         if (!GameBoardHelper.isWordNotGuessed(oldState.currentGuess, oldState.wordGuesses))
             throw new AlertKeyPressError("Word has already been guessed.");
 
+        // Get the status of the guess
         const currentGuessStatus = GameBoardHelper.getWordStatus(oldState.currentGuess, correctWord, oldState.revealedPositions);
 
+        // Check if the guess is correct
         const won = GameBoardHelper.isWordCorrect(oldState.currentGuess, correctWord);
 
+        // Return the new state
         return {
             ...oldState,
             wordGuesses: [...oldState.wordGuesses, oldState.currentGuess],
             currentGuess: "",
             wordGuessesStatus: [...oldState.wordGuessesStatus, currentGuessStatus],
             hasWon: won,
-            letterStatus: GameBoardHelper.newLetterStatus(oldState.letterStatus, oldState.currentGuess, currentGuessStatus),
+            letterStatus: GameBoardHelper.newLetterStatus(
+                oldState.letterStatus,
+                oldState.currentGuess,
+                currentGuessStatus,
+                oldState.revealedPositions,
+                correctWord,
+            ),
         };
     }
 
     static handleKeyPress (key: String, oldState: GameBoardState, correctWord: String, maxGuesses: Number): GameBoardState {
+        // If game is over (won or lost)
         if (oldState.hasWon)
             throw new InvalidKeyPressError("You have already won the game");
         if (oldState.wordGuesses.length >= maxGuesses)
@@ -71,6 +89,7 @@ export class GameBoardHandleKeyPressHelper {
         if (key === "Enter")
             return this.handleEnterKeyPress(oldState, correctWord);
 
+        // If key is not valid
         throw new InvalidKeyPressError("Invalid key press");
     }
 }
